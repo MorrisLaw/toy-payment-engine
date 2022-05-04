b0dcb72a5d10153cd223a76f7fbc8811dbc35aa6
Jeremy L. Morris <jeremylevanmorris@gmail.com>
2022-05-04 19:11:43 -0400

    payment engine can now take in csv of transactions and print values

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # toy-payment-engine
 Building a toy payment engine to learn more about Rust.
+
+## Development
+So far this tool can just read a csv file in as input. I've included a test input file for debugging purposes. Run the following commands from the root directory to test.
+
+- `cargo build` to build the code
+- `./target/debug/toy-payment-engine < test_input.csv` to run the engine with the test input csv as input
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use rust_decimal::Decimal;
+use serde::Deserialize;
+use std::io;
+
+#[derive(Deserialize)]
+struct Transaction {
+    id: u32,
+    client_id: u16,
+    kind: String,
+    amount: Decimal,
+}
+
+fn main() -> Result<(), csv::Error> {
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+
+    for tx in rdr.deserialize() {
+        let tx: Transaction = tx?;
+
+        match tx.kind.as_str(){
+            "deposit" => println!("ClientID: {}, TransactionID: {}, Kind: {}, Amount: {}", tx.client_id.clone(), tx.id.clone(), tx.kind.clone(), tx.amount.clone()),
+            "withdrawal" => println!("ClientID: {}, TransactionID: {}, Kind: {}, Amount: {}", tx.client_id.clone(), tx.id.clone(), tx.kind.clone(), tx.amount.clone()),
+            "dispute" => println!("ClientID: {}, TransactionID: {}, Kind: {}, Amount: {}", tx.client_id.clone(), tx.id.clone(), tx.kind.clone(), tx.amount.clone()),
+            "resolve" => println!("ClientID: {}, TransactionID: {}, Kind: {}, Amount: {}", tx.client_id.clone(), tx.id.clone(), tx.kind.clone(), tx.amount.clone()),
+            "chargeback" => println!("ClientID: {}, TransactionID: {}, Kind: {}, Amount: {}", tx.client_id.clone(), tx.id.clone(), tx.kind.clone(), tx.amount.clone()),
+            _ => println!("default"),
+        }
+    }
+
+    Ok(())
 }
