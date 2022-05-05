e9584b5dc254ef38bc489c1f4b0a761489ce47df
Jeremy L. Morris <jeremylevanmorris@gmail.com>
2022-05-05 09:53:20 -0400

    make it so that it can be run without needing < before the file name

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use rust_decimal::Decimal;
 use serde::Deserialize;
-use std::io;
+use std::env;
+use std::error::Error;
+use std::ffi::OsString;
+use std::fs::File;
+use std::process;
 
 #[derive(Deserialize)]
 struct Transaction {
@@ -10,8 +14,10 @@ struct Transaction {
     amount: Decimal,
 }
 
-fn main() -> Result<(), csv::Error> {
-    let mut rdr = csv::Reader::from_reader(io::stdin());
+fn run() -> Result<(), Box<dyn Error>> {
+    let file_path = get_first_arg()?;
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
 
     for tx in rdr.deserialize() {
         let tx: Transaction = tx?;
@@ -27,4 +33,18 @@ fn main() -> Result<(), csv::Error> {
     }
 
     Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
 }
